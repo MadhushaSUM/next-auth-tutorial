c33a3f0438d06e082b229eb1ec06d1c9094ea0ca
Madhusha Laksitha <madhusha.sum@gmail.com>
2024-03-10 21:20:23 +0530

    Extending auth session

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
+import { getUserById } from "@/data/user";
 
 
 export const {
@@ -10,6 +11,29 @@ export const {
     signIn,
     signOut,
 } = NextAuth({
+    callbacks: {
+        async session({token, session}) {
+            console.log(token);
+            
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+                session.user.role = token.role;
+            }
+            
+            return session;
+        },
+        async jwt({ token }) {   
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
+            
+            return token;
+        }
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
     ...authConfig,
